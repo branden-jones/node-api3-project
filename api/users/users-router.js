01194cfd3feb66ac956e3b024e84d60e1b96f114
Branden Jones <branden.wayne.jones@gmail.com>
2022-11-11 22:22:35 -0500

    post with id

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -2,6 +2,7 @@ const express = require('express');
 const {
   validateUserId,
   validateUser,
+  validatePost,
 } = require('../middleware/middleware');
 
 // You will need `users-model.js` and `posts-model.js` both
@@ -42,10 +43,12 @@ router.get('/:id/posts', validateUserId, (req, res) => {
   // this needs a middleware to verify user id
 });
 
-router.post('/:id/posts', validateUserId, (req, res) => {
+router.post('/:id/posts', validateUserId, validatePost, (req, res) => {
   // RETURN THE NEWLY CREATED USER POST
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
+  console.log(req.text)
+  console.log(req.user)
 });
 
 // do not forget to export the router
